add8c2b1396cf6b7778ebb92721140a1b6fb69e4
khushi zalavadiya <khushiz9959@gmail.com>
2024-08-19 18:24:59 +0530

    Base Model for Login/sign-up

--- app/src/main/java/com/example/project/TimeTable.java
+++ app/src/main/java/com/example/project/TimeTable.java
@@ -8,20 +8,25 @@ import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Login extends AppCompatActivity
+public class TimeTable extends AppCompatActivity
 {
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.login);
-        button=(Button)findViewById(R.id.button3);
+        setContentView(R.layout.timetable);
+        button=(Button)findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Login.this, TimeTable.class);
-                startActivity(intent);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent=new Intent(TimeTable.this, Attendence.class);
+                        startActivity(intent);
+                    }
+                });
             }
         });
     }
